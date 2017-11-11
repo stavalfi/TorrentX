7c484b17d00a9abc19bf3d3d0258f319c41d7180
stav alfi <stav.alfi@nice.com>
2017-11-11 19:43:18 +0200

    added a library for reading torrent files to pom.xml

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,13 +1,29 @@
 package main;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import christophedetroyer.torrent.Torrent;
+import christophedetroyer.torrent.TorrentFile;
+import christophedetroyer.torrent.TorrentParser;
+
+import java.io.IOException;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        Torrent t1= TorrentParser.parseTorrent("C:\\Users\\stava\\Desktop\\1.torrent");
+        System.out.println("Created By: " +t1.getCreatedBy());
+        System.out.println("Announce: " +t1.getAnnounce());
+        System.out.println("AnnounceList: " +t1.getAnnounceList());
+        System.out.println("Comment: " +t1.getComment());
+        System.out.println("Creation Date: " +t1.getCreationDate());
+        System.out.println("Info_hash: " +t1.getInfo_hash());
+        System.out.println("Name: " +t1.getName());
+        System.out.println("Piece Length: " +t1.getPieceLength());
+        System.out.println("Pieces: " +t1.getPieces());
+        System.out.println("Pieces Blob: " +t1.getPiecesBlob());
+        System.out.println("Total Size: " +t1.getTotalSize());
+        System.out.println("Is Single File Torrent: " +t1.isSingleFileTorrent());
+        System.out.println("File List: ");
+        t1.getFileList().forEach((TorrentFile torrentFile)-> System.out.println(torrentFile));
     }
 }
