f2c88bccf430cb7798de64ef826d95671464c4f0
stav alfi <stav.alfi@nice.com>
2017-11-11 20:51:29 +0200

    added a torrent file for experiments under src/main/resources

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,16 +4,23 @@ package main;
 import christophedetroyer.torrent.Torrent;
 import christophedetroyer.torrent.TorrentFile;
 import christophedetroyer.torrent.TorrentParser;
-
+import java.io.*;
+import java.net.*;
 import java.io.IOException;
 
 public class App
 {
     public static void main( String[] args ) throws IOException {
-        Torrent t1= TorrentParser.parseTorrent("C:\\Users\\stava\\Desktop\\1.torrent");
+        printTorrentFileInfo();
+    }
+
+    private static void printTorrentFileInfo() throws IOException
+    {
+        Torrent t1= TorrentParser.parseTorrent("src/main/resources/torrent-file-example.torrent");
         System.out.println("Created By: " +t1.getCreatedBy());
-        System.out.println("Announce: " +t1.getAnnounce());
-        System.out.println("AnnounceList: " +t1.getAnnounceList());
+        System.out.println("Main tracker: " +t1.getAnnounce());
+        System.out.println("Tracker List: ");
+        t1.getAnnounceList().forEach((String tracker)-> System.out.println(tracker));
         System.out.println("Comment: " +t1.getComment());
         System.out.println("Creation Date: " +t1.getCreationDate());
         System.out.println("Info_hash: " +t1.getInfo_hash());
