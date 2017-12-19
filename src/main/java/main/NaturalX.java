4f65dac2741614c53762d7e3d5cfdf4affe678b1
stav alfi <stav.alfi@nice.com>
2017-12-19 10:30:27 +0200

    communicating with peer - handshake - not working yet

--- src/main/java/main/NaturalX.java
+++ src/main/java/main/NaturalX.java
@@ -17,8 +17,12 @@ public class NaturalX {
             this.bytes.put((byte) ((value >> i * 8) | 0x00));
         }
     }
-    public ByteBuffer buffer()
-    {
+
+    public NaturalX(byte[] bytes) {
+        this.bytes = ByteBuffer.wrap(bytes);
+    }
+
+    public ByteBuffer buffer() {
         return this.bytes.asReadOnlyBuffer();
     }
 
@@ -29,34 +33,70 @@ public class NaturalX {
         }
         return value;
     }
+
     public static class Natural1 extends NaturalX {
         public Natural1(long value) {
             super(1, value);
         }
+
+        public Natural1(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==1;
+        }
     }
+
     public static class Natural2 extends NaturalX {
         public Natural2(long value) {
             super(2, value);
         }
+
+        public Natural2(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==2;
+        }
     }
+
     public static class Natural4 extends NaturalX {
         public Natural4(long value) {
             super(4, value);
         }
+
+        public Natural4(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==4;
+        }
     }
+
     public static class Natural5 extends NaturalX {
         public Natural5(long value) {
             super(5, value);
         }
+
+        public Natural5(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==5;
+        }
     }
+
     public static class Natural6 extends NaturalX {
         public Natural6(long value) {
             super(6, value);
         }
+
+        public Natural6(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==6;
+        }
     }
+
     public static class Natural7 extends NaturalX {
         public Natural7(long value) {
             super(7, value);
+        }
+
+        public Natural7(byte[] bytes) {
+            super(bytes);
+            assert bytes.length==7;
         }
     }
 }
