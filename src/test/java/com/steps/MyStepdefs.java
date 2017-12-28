e17c80a88b9186917c9d1b3887319aa7187859fe
stav alfi <stavalfi@gmail.com>
2017-12-28 22:45:22 +0200

    Added new feature file: PeersCommunication. There is one sceranio for now: send from application to a random peer a handshake request and I axpect to recieve a handshake response.

--- src/test/java/com/steps/MyStepdefs.java
+++ src/test/java/com/steps/MyStepdefs.java
@@ -3,13 +3,16 @@ package com.steps;
 import christophedetroyer.torrent.Torrent;
 import christophedetroyer.torrent.TorrentParser;
 import com.utils.CreateTrackerRequests;
-import com.utils.CreateTrackerRequests.RequestType;
-import cucumber.api.java.en.And;
+import com.utils.PeerMessageType;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import main.HexByteConverter;
+import main.Peer;
+import main.PeerCommunicator;
 import main.TrackerCommunicator;
+import main.peer.HandShake;
 import main.tracker.requests.AnnounceRequest;
 import main.tracker.requests.ConnectRequest;
 import main.tracker.requests.ScrapeRequest;
@@ -18,6 +21,7 @@ import main.tracker.response.ConnectResponse;
 import main.tracker.response.ScrapeResponse;
 import org.junit.Assert;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,10 +30,11 @@ import java.util.stream.Collectors;
 public class MyStepdefs {
 
     private Torrent torrentFile;
+    byte[] torrentInfoHashAsByteArray;
     private Map<String, Short> trackersConnectionInfo;
 
-    private Map.Entry<String, Short> activeTrackerWeCommunicate;
-    private final byte[] peerId = "-AZ5750-TpkXttZLfpSH".getBytes();
+    private Map.Entry<String, Short> activeTracker;
+    private final byte[] peerIdAsByteArray = "-AZ5750-TpkXttZLfpSH".getBytes();
     private final short tcpPortApplicationListenOn = 8091;
 
     private ConnectRequest connectRequest;
@@ -40,13 +45,18 @@ public class MyStepdefs {
     private AnnounceResponse announceResponse;
     private ScrapeResponse scrapeResponse;
 
-    @Given("^new torrent file: \"([^\"]*)\"$")
+    private Peer activePeer;
+
+    private HandShake handShakeRequest, handShakeResponse;
+
+    @Given("^new torrent file: \"([^\"]*)\".$")
     public void newTorrentFile(String torrentFilePath) throws Throwable {
         String torrentFilesLocation = "src/test/resources/";
         this.torrentFile = TorrentParser.parseTorrent(torrentFilesLocation + torrentFilePath);
+        this.torrentInfoHashAsByteArray = HexByteConverter.hexToByte(this.torrentFile.getInfo_hash());
     }
 
-    @When("^application read trackers for this torrent$")
+    @When("^application read trackers for this torrent.$")
     public void applicationReadTrackersForThisTorrent() throws Throwable {
         // tracker pattern example: udp://tracker.coppersurfer.tk:6969/announce
         String trackerPattern = "^udp://(.*):(\\d*)(.*)?$";
@@ -57,45 +67,7 @@ public class MyStepdefs {
                 .collect(Collectors.toMap((Matcher matcher) -> matcher.group(1), (Matcher matcher) -> Short.parseShort(matcher.group(2))));
     }
 
-    @When("^application send request: \"([^\"]*)\"$")
-    public void applicationSendRequest(RequestType requestType) throws Throwable {
-        byte[] torrentInfoHashAsByteArray = HexByteConverter.hexToByte(this.torrentFile.getInfo_hash());
-
-        // create and send a single demo request
-        CreateTrackerRequests requests = CreateTrackerRequests.getInstance();
-        switch (requestType) {
-            case CONNECT:
-                this.connectRequest = requests.createConnectRequest();
-                this.activeTrackerWeCommunicate = this.trackersConnectionInfo
-                        .entrySet()
-                        .stream()
-                        .findFirst()
-                        .get();
-                this.connectResponse = TrackerCommunicator.communicate(this.activeTrackerWeCommunicate.getKey(),
-                        activeTrackerWeCommunicate.getValue(),
-                        this.connectRequest);
-                break;
-            case ANNOUNCE:
-                assert this.connectResponse != null;
-                this.announceRequest = requests.createAnnounceRequest(this.connectResponse.getConnectionId(), torrentInfoHashAsByteArray,
-                        this.peerId, this.tcpPortApplicationListenOn);
-
-                this.announceResponse = TrackerCommunicator.communicate(this.activeTrackerWeCommunicate.getKey(),
-                        activeTrackerWeCommunicate.getValue(),
-                        this.announceRequest);
-                break;
-            case SCRAPE:
-                assert this.connectResponse != null;
-                assert this.announceResponse != null;
-                this.scrapeRequest = requests.createScrapeRequest(this.connectResponse.getConnectionId(), torrentInfoHashAsByteArray);
-                this.scrapeResponse = TrackerCommunicator.communicate(this.activeTrackerWeCommunicate.getKey(),
-                        activeTrackerWeCommunicate.getValue(),
-                        this.scrapeRequest);
-                break;
-        }
-    }
-
-    @Then("^tracker response with same transaction id$")
+    @Then("^tracker response with same transaction id.$")
     public void trackerResponseWithSameTransactionId() throws Throwable {
         if (this.connectRequest != null) {
             Assert.assertNotNull(this.connectResponse);
@@ -113,6 +85,95 @@ public class MyStepdefs {
             Assert.assertEquals(this.announceRequest.getTransactionId(), this.announceResponse.getTransactionId());
         }
 
+    }
+
+    @Then("^choose one tracker.$")
+    public void chooseOneTracker() throws Throwable {
+        assert this.trackersConnectionInfo.size() > 0;
+        this.activeTracker = this.trackersConnectionInfo
+                .entrySet()
+                .stream()
+                .findFirst()
+                .get();
+    }
+
+    @When("^application send tracker-request: CONNECT.$")
+    public void applicationSendTrackerRequestCONNECT() throws Throwable {
+        // create and send a single demo request
+        this.connectRequest = CreateTrackerRequests.getInstance().createConnectRequest();
+
+        this.connectResponse = TrackerCommunicator.communicate(this.activeTracker.getKey(),
+                activeTracker.getValue(),
+                this.connectRequest);
+    }
+
+    @When("^application send tracker-request: ANNOUNCE.$")
+    public void applicationSendRequestRequestANNOUNCE() throws Throwable {
+        // create and send a single demo request
+        assert this.connectResponse != null;
+        this.announceRequest = CreateTrackerRequests.getInstance().createAnnounceRequest(
+                this.connectResponse.getConnectionId(),
+                this.torrentInfoHashAsByteArray,
+                this.peerIdAsByteArray,
+                this.tcpPortApplicationListenOn);
+
+        this.announceResponse = TrackerCommunicator.communicate(this.activeTracker.getKey(),
+                activeTracker.getValue(),
+                this.announceRequest);
+    }
+
+    @When("^application send tracker-request: SCRAPE.$")
+    public void applicationSendRequestRequestSCRAPE() throws Throwable {
+        // create and send a single demo request
+        assert this.connectResponse != null;
+        assert this.announceResponse != null;
+        this.scrapeRequest = CreateTrackerRequests.getInstance().createScrapeRequest(
+                this.connectResponse.getConnectionId(),
+                this.torrentInfoHashAsByteArray);
+
+        this.scrapeResponse = TrackerCommunicator.communicate(this.activeTracker.getKey(),
+                activeTracker.getValue(),
+                this.scrapeRequest);
+    }
+
+    @Then("^choose one active peer to communicate with.$")
+    public void chooseOnePeer() throws Throwable {
+        this.handShakeRequest = new HandShake(this.torrentInfoHashAsByteArray, this.peerIdAsByteArray);
+        this.activePeer = this.announceResponse.getPeers()
+                .stream()
+                .filter((Peer peer) -> tryConnectPeer(peer))
+                .findFirst()
+                .get();
+    }
+
+    private boolean tryConnectPeer(Peer peer) {
+        try {
+            this.handShakeResponse = PeerCommunicator.sendMessage(
+                    peer.getIpAddress(),
+                    peer.getTcpPort(),
+                    this.handShakeRequest);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @When("^application send to peer a peer-request: \"([^\"]*)\".$")
+    public void applicationSendToPeerAPeerRequest(PeerMessageType peerMessageType) throws Throwable {
+
+    }
+
+    @Then("^application receive from peer a peer-response: \"([^\"]*)\".$")
+    public void applicationReceiveFromPeerAPeerResponse(PeerMessageType peerMessageType) throws Throwable {
+        switch (peerMessageType) {
+            case HANDSHAKE:
+                Assert.assertNotNull(this.handShakeResponse);
+                String torrentInfoHashFromRequest = HexByteConverter.byteToHex(this.handShakeRequest.getTorrentInfoHash());
+                String torrentInfoHashFromResponse = HexByteConverter.byteToHex(this.handShakeResponse.getTorrentInfoHash());
+                Assert.assertEquals(torrentInfoHashFromRequest, torrentInfoHashFromResponse);
+                break;
+        }
     }
 }
 
--- src/test/resources/cucumber/TrackerCommunication.feature
+++ src/test/resources/cucumber/TrackerCommunication.feature
@@ -1,17 +1,18 @@
-Feature: test tracker api calls
+Feature: test tracker api calls.
 
   Scenario Outline: test connection, announce and scrape requests and responses to one of the trackers.
-    Given new torrent file: "<torrentFilePath>"
-    When application read trackers for this torrent
+    Given new torrent file: "<torrentFilePath>".
+    When application read trackers for this torrent.
+    Then choose one tracker.
 
-    When application send request: "CONNECT"
-    Then tracker response with same transaction id
+    When application send tracker-request: CONNECT.
+    Then tracker response with same transaction id.
 
-    When application send request: "ANNOUNCE"
-    Then tracker response with same transaction id
+    When application send tracker-request: ANNOUNCE.
+    Then tracker response with same transaction id.
 
-    When application send request: "SCRAPE"
-    Then tracker response with same transaction id
+    When application send tracker-request: SCRAPE.
+    Then tracker response with same transaction id.
     Examples:
       | torrentFilePath               |
       | torrent-file-example1.torrent |
