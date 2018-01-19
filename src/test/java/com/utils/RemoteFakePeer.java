9f27d8918140628b4eb5bbf7df844704ccbd4cb1
stav alfi <stavalfi@gmail.com>
2018-01-19 21:46:13 +0200

    fixed a bug - this class use now listenToPeerConnection socket (the class used other socket by mistake).

--- src/test/java/com/utils/RemoteFakePeer.java
+++ src/test/java/com/utils/RemoteFakePeer.java
@@ -32,16 +32,14 @@ public class RemoteFakePeer extends Peer {
 
     public void listen() {
         new Thread(() -> {
-            ServerSocket welcomeSocket = null;
             try {
-                welcomeSocket = new ServerSocket(6789);
                 while (!this.closeEverything) {
-                    Socket newPeerConnection = welcomeSocket.accept();
+                    Socket newPeerConnection = this.listenToPeerConnection.accept();
                     this.peerConnections.add(newPeerConnection);
                     waitForMessagesFromPeer(newPeerConnection);
                 }
             } catch (Exception e) {
-
+                e.printStackTrace();
             }
         }).start();
     }
