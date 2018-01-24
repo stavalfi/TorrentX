56f0982487c1f56dc9f19684eaa3b34d35ed3a93
stav alfi <stavalfi@gmail.com>
2018-01-24 22:54:58 +0200

    I added new method inside PeerMessageFactory which get InputStream so we can build a PeerMessage from a socket response.

--- src/main/java/main/peer/PeerMessageFactory.java
+++ src/main/java/main/peer/PeerMessageFactory.java
@@ -2,6 +2,10 @@ package main.peer;
 
 import main.peer.peerMessages.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
 public class PeerMessageFactory {
     private static final int bitFieldMessage = 5;
     private static final int cancelMessage = 8;
@@ -14,6 +18,22 @@ public class PeerMessageFactory {
     private static final int portMessage = 9;
     private static final int requestMessage = 6;
     private static final int unchokeMessage = 1;
+
+    public static PeerMessage create(Peer from, Peer to, InputStream inputStream) throws IOException {
+        byte[] data = new byte[4];
+        boolean isPeerClosedConnection = (inputStream.read(data) == -1);
+        if (isPeerClosedConnection)
+            throw new IOException("the peer closed the socket:" + from.toString());
+        int lengthOfTheRest = ByteBuffer.wrap(data).getInt();// how much do we need to read more
+        data = new byte[lengthOfTheRest];
+        isPeerClosedConnection = (inputStream.read(data) == -1);
+        if (isPeerClosedConnection)
+            throw new IOException("the peer closed the socket:" + from.toString());
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4 + lengthOfTheRest);
+        byteBuffer.putInt(lengthOfTheRest);
+        byteBuffer.put(data);
+        return create(from, to, byteBuffer.array());
+    }
 
     public static PeerMessage create(Peer from, Peer to, byte[] peerMessage) {
         int messageId = PeerMessage.getMessageId(peerMessage);
