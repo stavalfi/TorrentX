2d5a4f2da960a174e8a7e59df5cad2a38f3ce1cd
stav alfi <stavalfi@gmail.com>
2018-03-02 15:27:10 +0200

    created interface for getting each seperate peer-message-type as a unique flux.

---
+++ src/main/java/main/peer/ReceivePeerMessages.java
@@ -0,0 +1,30 @@
+package main.peer;
+
+import main.peer.peerMessages.*;
+import reactor.core.publisher.Flux;
+
+public interface ReceivePeerMessages {
+    Flux<BitFieldMessage> getBitFieldMessageResponseFlux();
+
+    Flux<CancelMessage> getCancelMessageResponseFlux();
+
+    Flux<ChokeMessage> getChokeMessageResponseFlux();
+
+    Flux<ExtendedMessage> getExtendedMessageResponseFlux();
+
+    Flux<HaveMessage> getHaveMessageResponseFlux();
+
+    Flux<InterestedMessage> getInterestedMessageResponseFlux();
+
+    Flux<KeepAliveMessage> getKeepMessageResponseFlux();
+
+    Flux<NotInterestedMessage> getNotInterestedMessageResponseFlux();
+
+    Flux<PieceMessage> getPieceMessageResponseFlux();
+
+    Flux<PortMessage> getPortMessageResponseFlux();
+
+    Flux<RequestMessage> getRequestMessageResponseFlux();
+
+    Flux<UnchokeMessage> getUnchokeMessageResponseFlux();
+}
