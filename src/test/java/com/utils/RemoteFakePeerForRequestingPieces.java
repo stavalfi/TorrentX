a6b477e12cab73090901e6fc847cabcf09c3b2e0
stav alfi <stavalfi@gmail.com>
2018-04-06 23:29:55 +0300

    tests: Removed unnecessary code from RemoteFakePeerForRequestingPieces class.

--- src/test/java/com/utils/RemoteFakePeerForRequestingPieces.java
+++ src/test/java/com/utils/RemoteFakePeerForRequestingPieces.java
@@ -2,33 +2,18 @@ package com.utils;
 
 import main.peer.PeersCommunicator;
 import main.peer.peerMessages.PeerMessage;
-import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 public class RemoteFakePeerForRequestingPieces {
 
-    private Disposable subscribe1;
-    private Disposable subscribe2;
-    private Disposable subscribe3;
     private PeersCommunicator peersCommunicator;
-    private AtomicBoolean isInterestedInMe = new AtomicBoolean(false);
 
     public RemoteFakePeerForRequestingPieces(PeersCommunicator peersCommunicator) {
         this.peersCommunicator = peersCommunicator;
-        this.subscribe1 = this.peersCommunicator.receivePeerMessages()
-                .getInterestedMessageResponseFlux()
-                .subscribe(interestedMessage -> this.isInterestedInMe.set(true));
 
-        this.subscribe2 = this.peersCommunicator.receivePeerMessages()
-                .getNotInterestedMessageResponseFlux()
-                .subscribe(interestedMessage -> this.isInterestedInMe.set(false));
-
-        this.subscribe3 = this.peersCommunicator.receivePeerMessages()
+        this.peersCommunicator.receivePeerMessages()
                 .getRequestMessageResponseFlux()
-                .filter(requestMessage -> this.isInterestedInMe.get())
                 .flatMap(requestMessage ->
                         this.peersCommunicator.sendPieceMessage(requestMessage.getIndex(),
                                 requestMessage.getBegin(),
@@ -56,12 +41,5 @@ public class RemoteFakePeerForRequestingPieces {
         for (int i = 0; i < length; i++, content++)
             bytes[i] = content;
         return bytes;
-    }
-
-    public synchronized void closeConnection() {
-        this.peersCommunicator.closeConnection();
-        this.subscribe1.dispose();
-        this.subscribe2.dispose();
-        this.subscribe3.dispose();
     }
 }
