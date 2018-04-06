cbb3f4c697ab324f7e83c8036f0fdcde2c6fad13
stav alfi <stavalfi@gmail.com>
2018-04-07 02:40:24 +0300

    SendPeerMessage interface: Added 2 overloads to be able to send a message by a pre-defined message.

--- src/main/java/main/peer/SendPeerMessage.java
+++ src/main/java/main/peer/SendPeerMessage.java
@@ -1,6 +1,8 @@
 package main.peer;
 
+import main.peer.peerMessages.BitFieldMessage;
 import main.peer.peerMessages.PeerMessage;
+import main.peer.peerMessages.RequestMessage;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -8,6 +10,8 @@ import java.util.BitSet;
 
 public interface SendPeerMessage {
     Mono<PeersCommunicator> sendBitFieldMessage(BitSet peaces);
+
+    Mono<PeersCommunicator> sendBitFieldMessage(BitFieldMessage bitFieldMessage);
 
     Mono<PeersCommunicator> sendCancelMessage(int index, int begin, int length);
 
@@ -26,6 +30,8 @@ public interface SendPeerMessage {
     Mono<PeersCommunicator> sendPortMessage(short listenPort);
 
     Mono<PeersCommunicator> sendRequestMessage(int index, int begin, int length);
+
+    Mono<PeersCommunicator> sendRequestMessage(RequestMessage requestMessage);
 
     Mono<PeersCommunicator> sendUnchokeMessage();
 
