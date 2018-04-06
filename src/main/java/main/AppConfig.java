9cb1cdd7475061c626b828a8312ec8aee5a467b3
stav alfi <stavalfi@gmail.com>
2018-04-06 15:48:11 +0300

    1. I wrote the port which the app listen for new peers through him in the class: AppConfig. 2. Inisde the class PeersListener, I implemented the method: haveThisTorrent.

--- src/main/java/main/AppConfig.java
+++ src/main/java/main/AppConfig.java
@@ -2,6 +2,7 @@ package main;
 
 public class AppConfig {
     private String peerId = "-AZ5750-TpkXttZLfpSH";
+    private int myListeningPort=9191;
 
     private static AppConfig instance = new AppConfig();
 
@@ -15,5 +16,9 @@ public class AppConfig {
 
     public String getPeerId() {
         return peerId;
+    }
+
+    public int getMyListeningPort() {
+        return myListeningPort;
     }
 }
