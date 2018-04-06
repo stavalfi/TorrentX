b27eec875bd2ea94fb20c4e2d354e790a42d7319
stav alfi <stavalfi@gmail.com>
2018-04-06 23:34:27 +0300

    I implemented the logic of the tests for the marked scenarios inside CommunicateWithFakePeers.feature

--- src/main/java/main/algorithms/BittorrentAlgorithmImpl.java
+++ src/main/java/main/algorithms/BittorrentAlgorithmImpl.java
@@ -1,34 +1,51 @@
 package main.algorithms;
 
-import main.TorrentInfo;
 import main.downloader.TorrentPieceChanged;
+import main.file.system.PieceNotDownloadedYetException;
+import main.file.system.TorrentFileSystemManager;
 import main.peer.PeersCommunicator;
 import main.peer.ReceivedMessagesImpl;
 import main.torrent.status.TorrentStatus;
 import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public class BittorrentAlgorithmImpl implements BittorrentAlgorithm {
     private Flux<TorrentPieceChanged> startDownloadFlux;
 
-    public BittorrentAlgorithmImpl(TorrentInfo torrentInfo,
-                                   TorrentStatus torrentStatus,
+    public BittorrentAlgorithmImpl(TorrentStatus torrentStatus,
+                                   TorrentFileSystemManager torrentFileSystemManager,
                                    Flux<PeersCommunicator> peersCommunicatorFlux) {
-
-        ReceivedMessagesImpl receiveTorrentMessagesMessagesFlux = new ReceivedMessagesImpl(
-                peersCommunicatorFlux.map(PeersCommunicator::receivePeerMessages));
-
-        this.startDownloadFlux = startBittorrentAlgorithm(torrentInfo, torrentStatus, peersCommunicatorFlux)
+        this.startDownloadFlux = downloadFlux(torrentStatus,
+                torrentFileSystemManager, peersCommunicatorFlux)
                 .publish();
 
         ((ConnectableFlux<TorrentPieceChanged>) this.startDownloadFlux).connect();
 
+        uploadFlux(torrentStatus, torrentFileSystemManager, peersCommunicatorFlux)
+                .publish()
+                .connect();
     }
 
-    private Flux<TorrentPieceChanged> startBittorrentAlgorithm(TorrentInfo torrentInfo,
-                                                               TorrentStatus torrentStatus,
-                                                               Flux<PeersCommunicator> peersCommunicatorFlux) {
+    private Flux<TorrentPieceChanged> downloadFlux(TorrentStatus torrentStatus,
+                                                   TorrentFileSystemManager torrentFileSystemManager,
+                                                   Flux<PeersCommunicator> peersCommunicatorFlux) {
+        ReceivedMessagesImpl receiveTorrentMessagesMessagesFlux = new ReceivedMessagesImpl(
+                peersCommunicatorFlux.map(PeersCommunicator::receivePeerMessages));
+
         return Flux.empty();
+    }
+
+    private Flux<PeersCommunicator> uploadFlux(TorrentStatus torrentStatus,
+                                               TorrentFileSystemManager torrentFileSystemManager,
+                                               Flux<PeersCommunicator> peersCommunicatorFlux) {
+        return peersCommunicatorFlux.flatMap(peersCommunicator ->
+                peersCommunicator.receivePeerMessages()
+                        .getRequestMessageResponseFlux()
+                        .flatMap(requestMessage -> torrentFileSystemManager.buildPieceMessage(requestMessage)
+                                .onErrorResume(PieceNotDownloadedYetException.class, throwable -> Mono.empty()))
+                        .flatMap(pieceMessage -> peersCommunicator.sendPieceMessage(pieceMessage.getIndex(),
+                                pieceMessage.getBegin(), pieceMessage.getBlock())));
     }
 
     @Override
