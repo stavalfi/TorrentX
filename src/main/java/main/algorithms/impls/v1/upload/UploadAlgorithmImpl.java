4b59cba3a6ab4b30c0abbba87af40932081f3f43
stav alfi <stavalfi@gmail.com>
2018-04-22 00:40:22 +0300

    Added note to UploadAlgorithmImpl class about a possible duplicate bug in the future.

--- src/main/java/main/algorithms/impls/v1/upload/UploadAlgorithmImpl.java
+++ src/main/java/main/algorithms/impls/v1/upload/UploadAlgorithmImpl.java
@@ -18,30 +18,30 @@ public class UploadAlgorithmImpl implements UploadAlgorithm {
     private Flux<PieceEvent> uploadedBlocksFlux;
 
     public UploadAlgorithmImpl(TorrentInfo torrentInfo,
-                        TorrentStatus torrentStatus,
-                        TorrentFileSystemManager torrentFileSystemManager,
-                        Flux<Link> peersCommunicatorFlux) {
+                               TorrentStatus torrentStatus,
+                               TorrentFileSystemManager torrentFileSystemManager,
+                               Flux<Link> peersCommunicatorFlux) {
         this.torrentInfo = torrentInfo;
         this.torrentStatus = torrentStatus;
         this.torrentFileSystemManager = torrentFileSystemManager;
         this.peersCommunicatorFlux = peersCommunicatorFlux;
 
-        this.uploadedBlocksFlux =
-                this.torrentStatus
-                        .notifyWhenStartedUploading()
-                        .flatMapMany(__ -> this.peersCommunicatorFlux)
-                        .flatMap(peersCommunicator ->
-                                peersCommunicator.receivePeerMessages()
-                                        .getRequestMessageResponseFlux()
-                                        .filter(requestMessage -> this.torrentFileSystemManager.havePiece(requestMessage.getIndex()))
-                                        .flatMap(requestMessage -> this.torrentStatus.notifyWhenResumeUpload()
-                                                .flatMap(___ -> this.torrentFileSystemManager.buildPieceMessage(requestMessage)))
-                                        .flatMap(pieceMessage ->
-                                                peersCommunicator.sendMessages().sendPieceMessage(pieceMessage.getIndex(),
-                                                        pieceMessage.getBegin(), pieceMessage.getBlock())
-                                                        .map(___ -> new PieceEvent(TorrentPieceStatus.UPLOADING, pieceMessage))))
-                        .publish()
-                        .autoConnect(0);
+        this.uploadedBlocksFlux = this.torrentStatus
+                .notifyWhenStartUploading()
+                .flatMapMany(__ -> this.peersCommunicatorFlux)
+                .flatMap(peersCommunicator ->
+                        // if getRequestMessageResponseFlux is not running on unique thread, then I will block
+                        // notifyWhenStartUploading() thread. Watch out.
+                        peersCommunicator.receivePeerMessages().getRequestMessageResponseFlux()
+                                .filter(requestMessage -> this.torrentFileSystemManager.havePiece(requestMessage.getIndex()))
+                                .flatMap(requestMessage -> this.torrentStatus.notifyWhenResumeUpload()
+                                        .flatMap(___ -> this.torrentFileSystemManager.buildPieceMessage(requestMessage)))
+                                .flatMap(pieceMessage ->
+                                        peersCommunicator.sendMessages().sendPieceMessage(pieceMessage.getIndex(),
+                                                pieceMessage.getBegin(), pieceMessage.getBlock())
+                                                .map(___ -> new PieceEvent(TorrentPieceStatus.UPLOADING, pieceMessage))))
+                .publish()
+                .autoConnect(0);
     }
 
     @Override
