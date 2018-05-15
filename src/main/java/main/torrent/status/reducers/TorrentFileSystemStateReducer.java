48327cb3971d46a425ad3d8add8b6541c6ce5c85
stav alfi <stavalfi@gmail.com>
2018-05-15 19:04:00 +0300

    implemented TorrentFileSystemStateReducer

--- src/main/java/main/torrent/status/reducers/TorrentFileSystemStateReducer.java
+++ src/main/java/main/torrent/status/reducers/TorrentFileSystemStateReducer.java
@@ -15,7 +15,56 @@ public class TorrentFileSystemStateReducer {
                     .setTorrentRemovedWindUp(false)
                     .build();
 
-    public TorrentFileSystemState reducer(TorrentStatusState oldState, Action action) {
-        return oldState.getTorrentFileSystemState();
+    public TorrentFileSystemState reducer(TorrentStatusState lastState, Action action) {
+        TorrentFileSystemState torrentFileSystemState = lastState.getTorrentFileSystemState();
+
+        boolean isDownloadingOrUploadingOrInProgress = lastState.getDownloadState().isStartDownloadInProgress() ||
+                lastState.getDownloadState().isStartDownloadWindUp() ||
+                lastState.getDownloadState().isStartUploadInProgress() ||
+                lastState.getDownloadState().isStartUploadWindUp();
+
+        boolean areWeListeningOrSearchingOrInProgress = lastState.getPeersState().isResumeListeningToIncomingPeersInProgress() ||
+                lastState.getPeersState().isResumeListeningToIncomingPeersWindUp() ||
+                lastState.getPeersState().isResumeSearchingPeersInProgress() ||
+                lastState.getPeersState().isResumeSearchingPeersWindUp();
+
+        switch (action) {
+            case REMOVE_FILES_IN_PROGRESS:
+                if (torrentFileSystemState.isFilesRemovedInProgress() ||
+                        torrentFileSystemState.isFilesRemovedWindUp())
+                    return torrentFileSystemState;
+                return TorrentFileSystemState.TorrentFileSystemStateBuilder.builder()
+                        .setFilesRemovedInProgress(true)
+                        .build();
+            case REMOVE_FILES_WIND_UP:
+                if (isDownloadingOrUploadingOrInProgress ||
+                        areWeListeningOrSearchingOrInProgress ||
+                        !torrentFileSystemState.isFilesRemovedInProgress() ||
+                        torrentFileSystemState.isFilesRemovedWindUp())
+                    return torrentFileSystemState;
+                return TorrentFileSystemState.TorrentFileSystemStateBuilder.builder()
+                        .setFilesRemovedInProgress(false)
+                        .setFilesRemovedWindUp(true)
+                        .build();
+            case REMOVE_TORRENT_IN_PROGRESS:
+                if (torrentFileSystemState.isTorrentRemovedInProgress() ||
+                        torrentFileSystemState.isTorrentRemovedWindUp())
+                    return torrentFileSystemState;
+                return TorrentFileSystemState.TorrentFileSystemStateBuilder.builder()
+                        .setTorrentRemovedInProgress(true)
+                        .build();
+            case REMOVE_TORRENT_WIND_UP:
+                if (isDownloadingOrUploadingOrInProgress ||
+                        areWeListeningOrSearchingOrInProgress ||
+                        !torrentFileSystemState.isTorrentRemovedInProgress() ||
+                        torrentFileSystemState.isTorrentRemovedWindUp())
+                    return torrentFileSystemState;
+                return TorrentFileSystemState.TorrentFileSystemStateBuilder.builder()
+                        .setTorrentRemovedInProgress(false)
+                        .setTorrentRemovedWindUp(true)
+                        .build();
+        }
+
+        return lastState.getTorrentFileSystemState();
     }
 }
